0a2cf10f1afe7b1211063c1eae011f66a9a98158
DESKTOP-DVPF2K2\U <matveychukivan.r@gmail.com>
2021-12-21 07:02:18 +0300

    add authorization

---
+++ communication/src/main/java/ru/ifmo/email/communication/ICommand.java
@@ -0,0 +1,9 @@
+package ru.ifmo.email.communication;
+
+import ru.ifmo.email.model.User;
+
+import java.io.Serializable;
+
+// Команда, которая будет использоваться для общения клиента и сервера.
+public interface ICommand extends Serializable {
+}
